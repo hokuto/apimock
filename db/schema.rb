656d988641de721053ce81268a157a024f2862b2
Hokuto Yamada <hokuto.yamada.j@gmail.com>
2020-08-21 01:01:57 +0900

    Procfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_08_20_153315) do
 
-  create_table "api_responses", force: :cascade do |t|
+  create_table "api_responses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "json"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
